b1de52677b9a489f02fa54ae5ca80fcebe72c3e6
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-10 16:34:02 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-for="text in 3" :key="text.id" class="flex bg-white w-[650] shadow mx-auto mt-2">
+  <div v-for="post in postData" 
+  :key="post.id" 
+  class="flex bg-white w-[650] shadow mx-auto mt-2">
     <div class="p-2 flex-shrink-0">
-        <img class="shadow" height="250" width="250" src="https://picsum.photos/536/354" alt="img1">
+        <img class="shadow" height="250" width="250" :src="post.img" alt="img1">
     </div>
     <div class="ml-2 p-2">
-      <div class="font-bold text-lg text-purple-600">กรุงเทพมหานคร</div>
+      <div class="font-bold text-lg text-purple-600">{{ post.title }}</div>
       <div class="text-sm">
-       
+       {{ post.body }}
       </div>
     </div>
   </div>
@@ -14,14 +16,20 @@
 
 <script>
 import axios from "axios";
-import { onMounted } from '@vue/runtime-core';
+import { onMounted, ref } from '@vue/runtime-core';
 
 export default {
   name: "HomePage",
   setup(){
+    const postData = ref(null)
     onMounted(()=>{
-      axios.get("http://localhost:3000/posts").then((res)=>console.log(res))
+      axios
+      .get("http://localhost:3000/posts")
+      .then((res)=>(postData.value = res.data))
+
+      console.log(postData)
     })
+    return {postData}
   }
 };
 </script>
