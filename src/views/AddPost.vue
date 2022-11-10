af1e2bc51261a8bc30219fd67098ee799dfd06f7
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-10 17:05:01 +0700

    update

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    <h1 class="text-2xl text-teal-500">Add Post</h1>
+  <div class="bg-white flex flex-col w-80 p-2 m-2 shadow space-y-2">
+    <div>
+      <label class="block" for="">เรื่อง</label>
+      <input class="border w-full" type="text" />
+    </div>
+
+    <div>
+      <label class="block" for="">ลิงค์ URL</label>
+      <input class="border w-full" type="text" />
+    </div>
+
+    <div>
+      <label class="block" for="">รายละเอียด</label>
+      <textarea class="border w-full" rows="5"></textarea>
+    </div>
+
+    <button class="bg-green-400 text-white py-2">บันทึก</button>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
