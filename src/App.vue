a03f6384810f6e5f89421dac539a6881a85816f5
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-10 15:46:20 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
+  
+  <div class="text-lg bg-red-500 text-white p-4 shadow">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+
   <RouterView />
 </template>
+
+<script>
+  export default{
+    name:"Vue3-Blog"
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>
-        <h1>Home page</h1>
+  <div class="flex bg-white w-[650] shadow mx-auto mt-2">
+    <div class="p-2 flex-shrink-0">
+        <img class="shadow" height="250" width="250" src="https://picsum.photos/536/354" alt="img1">
     </div>
+    <div class="ml-2 p-2">
+      <div class="font-bold text-lg text-purple-600">กรุงเทพมหานคร</div>
+      <div class="text-sm">
+        กรุงเทพมหานคร เป็นเมืองหลวงและนครที่มีประชากรมากที่สุดของประเทศไทย
+        เป็นศูนย์กลางการปกครอง การศึกษา การคมนาคมขนส่ง การเงินการธนาคาร
+        การพาณิชย์ การสื่อสาร และความเจริญของประเทศ
+      </div>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
-    name: "HomePage",
-}
+  name: "HomePage",
+};
 </script>
-<style>
-    
-</style>
+<style></style>
