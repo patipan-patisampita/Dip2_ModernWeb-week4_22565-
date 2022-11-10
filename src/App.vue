c8617f085a6953bd0297b274218c2b14c5e277b2
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-10 16:54:43 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   
-  <div class="text-lg bg-red-500 text-white p-4 shadow">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="text-lg bg-red-500 text-white h-10 flex items-center mx-auto px-4 shadow">
+    <router-link to="/" class="flex items-center h-10 px-2">Home</router-link> |
+    <router-link to="/addpost" class="flex items-center h-10 px-2">Add Post</router-link> |
+    <router-link to="/about" class="flex items-center h-10 px-2">About</router-link>
   </div>
 
   <RouterView />
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import AddPost from '@/views/AddPost.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
             path: '/',
             name: "home",
             component: Home,
+        },
+        {
+            path: '/addpost',
+            name: "addpost",
+            component: AddPost,
         },
         {
             path: '/about',
